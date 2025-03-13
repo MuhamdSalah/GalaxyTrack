55f8b457c64f86f64659b4974a8491d0b91092d9
MuhamdSalah <salahmohamedabdelhafez@gmail.com>
2025-03-13 13:18:17 +0200

    Add files via upload

---
+++ SpaceTrack.DAL/DTOs/PayloadDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTrack.DAL.DTOs
+{
+    public class PayloadDto
+    {
+        public string ObjectName { get; set; }
+        public int NoradCatId { get; set; }
+    }
+}
---
+++ SpaceTrack.DAL/DTOs/SpaceTrackSatellite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTrack.DAL.DTOs
+{
+    using System.Text.Json.Serialization;
+
+    public class SpaceTrackSatellite
+    {
+        [JsonPropertyName("NORAD_CAT_ID")]
+        public int NoradId { get; set; }
+
+        [JsonPropertyName("OBJECT_NAME")]
+        public string ObjectName { get; set; }
+    }
+
+}
